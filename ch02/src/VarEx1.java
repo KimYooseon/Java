d68e4d7cae2596c6f446dfdda7563abf44478e61
smir1997 <45276885+KimYooseon@users.noreply.github.com>
2020-02-28 11:33:37 +0900

    Add files via upload

---
+++ ch02/src/VarEx1.java
@@ -0,0 +1,10 @@
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world.");
+		System.out.print("3+5=");
+		System.out.println(3+5);
+	}
+}
