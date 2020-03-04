4966aca102f6e431c69ee50e4c016ae3235f1514
smir1997 <45276885+KimYooseon@users.noreply.github.com>
2020-03-04 19:40:55 +0900

    Add files via upload

--- ch03/src/Ex3_11.java
+++ ch03/src/Ex3_11.java
@@ -3,8 +3,12 @@ public class Ex3_11 {
 
 	public static void main(String[] args) {
 		double pi = 3.141592;
-		double shortPi = Math.round(pi * 1000) / 1000.0;
+		double shortPi = Math.round(pi * 1000) / 1000.0; // 반올림
 		System.out.println(shortPi);
+		
+		shortPi = Math.round((int)(pi * 1000)) / 1000.0; // 내림(실수가 정수로 바뀌면서 소수점자리가 탈락됨)
+		System.out.println(shortPi);
+		
 
 	}
 
