de6af3a0408ae239756c6019411e9b7643428993
smir1997 <45276885+KimYooseon@users.noreply.github.com>
2020-03-08 07:35:20 +0900

    Add files via upload

---
+++ ch04/src/Ex4_15.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ex4_15 {
+
+	public static void main(String[] args) {
+		int input = 0, answer = 0;
+		
+		answer = (int)(Math.random()*100)+1;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(answer);
+		do {
+			System.out.printf("1과 100사이의 정수를 입력하세요.>");
+			input = scanner.nextInt();
+			
+		 if (answer > input) {
+			System.out.println("더 큰 수로 시도해보세요.");
+		 } else if (answer < input){
+			 System.out.println("더 작은 수로 시도해보세요");
+		 }
+		} while(answer != input);
+		System.out.println("정답입니다.");
+	}
+
+}
