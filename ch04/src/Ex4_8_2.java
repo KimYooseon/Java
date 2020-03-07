d40da4c4d732f8ecb721cb992deea845ce0402de
smir1997 <45276885+KimYooseon@users.noreply.github.com>
2020-03-07 21:36:33 +0900

    Add files via upload

--- ch04/src/Ex4_8_2.java
+++ ch04/src/Ex4_8_2.java
@@ -3,7 +3,9 @@ public class Ex4_8_2 {
 
 	public static void main(String[] args) {
 		for (int i = 1; i<=9; i++) {
-			System.out.println(i + "x" + i + "=" + i * i);
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(i + "x" + j + "=" + i * j);
+			}System.out.println();
 		}
 
 	}
