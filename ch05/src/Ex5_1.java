ba19362a5a29533c317742d505074959260aeeb4
smir1997 <45276885+KimYooseon@users.noreply.github.com>
2020-03-12 05:42:44 +0900

    Add files via upload

--- ch05/src/Ex5_1.java
+++ ch05/src/Ex5_1.java
@@ -5,8 +5,8 @@ public class Ex5_1 {
 	public static void main(String[] args) {
 		int[] iArr1 = new int[10];
 		int[] iArr2 = new int[10];
-//		int[] iArr3 = new int[] {100,90,80,70,60};
-		int[] iArr3 = {100, 90, 80, 70, 60};
+		int[] iArr3 = {100, 95, 80, 70, 60};
+//		int[] iArr3 = new int[] {100,90,80,70,60};  // 이렇게 해도 됨
 		char[] chArr = {'a', 'b', 'c', 'd'};
 		
 		for(int i=0; i<iArr1.length; i++) {
